147f2386fb9fdfab0653ffb6f39a514fbd6da0d6
Simon Mitchell <simonmitchell@Simons-MacBook.local>
2020-02-14 18:25:37 +0000

    Error logs are now combined in one .log file

--- src/winston.ts
+++ src/winston.ts
@@ -1,12 +1,11 @@
-import winston from 'winston';
+import winston, { loggers } from 'winston';
 
 const logger = winston.createLogger({
-    level: 'info',
     format: winston.format.json(),
     defaultMeta: {},
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: './logs/error.log' }),
+        new winston.transports.File({ filename: './logs/combined.log' }),
     ],
 });
 
